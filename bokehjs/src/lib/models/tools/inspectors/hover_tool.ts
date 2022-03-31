willsketch/bokeh

commit 3f4a617c70d264e15e70bb3ca263e55657298ceb

Hover tooltips for Patch and Areas (#11992)
* Initial functioning WIP

* WIP Patch and HArea

* check for potentially null value

* Remove commented out code

* remove unnecessary to_screen call

* break after successful polygon hit test

* Add a comment regarding selection.is_empty()

Co-authored-by: Mateusz Paprocki <mattpap@gmail.com>

bokehjs/src/lib/models/tools/inspectors/hover_tool.ts
@@ -8,6 +8,9 @@ import {GraphRenderer} from "../../renderers/graph_renderer"
 import {DataRenderer} from "../../renderers/data_renderer"
 import {LineView} from "../../glyphs/line"
 import {MultiLineView} from "../../glyphs/multi_line"
+import {PatchView} from "../../glyphs/patch"
+import {HAreaView} from "../../glyphs/harea"
+import {VAreaView} from "../../glyphs/varea"
 import * as hittest from "core/hittest"
 import {MoveEvent} from "core/ui_events"
 import {replace_placeholders, Formatters, FormatterType, Vars} from "core/util/templating"
@@ -29,7 +32,7 @@ import {compute_renderers} from "../../util"
 import * as styles from "styles/tooltips.css"
 import {Template, TemplateView} from "../../dom"
 
-export type TooltipVars = {index: number} & Vars
+export type TooltipVars = {index?: number} & Vars
 
 export function _nearest_line_hit(i: number, geometry: Geometry,
     sx: number, sy: number, dx: Arrayable<number>, dy: Arrayable<number>): [[number, number], number] {
@@ -224,7 +227,8 @@ export class HoverToolView extends InspectToolView {
     const fullset_indices = selection_manager.inspectors.get(renderer)!
     const subset_indices = renderer.view.convert_selection_to_subset(fullset_indices)
 
-    if (fullset_indices.is_empty()) {
+    // XXX: https://github.com/bokeh/bokeh/pull/11992#pullrequestreview-897552484
+    if (fullset_indices.is_empty() && fullset_indices.view == null) {
       tooltip.clear()
       return
     }
@@ -243,6 +247,47 @@ export class HoverToolView extends InspectToolView {
     const {glyph} = renderer_view
 
     const tooltips: [number, number, HTMLElement | null][] = []
+
+    if (glyph instanceof PatchView) {
+      const [rx, ry] = [sx, sy]
+      const vars = {
+        x, y, sx, sy, rx, ry,
+        name: renderer.name,
+      }
+      tooltips.push([rx, ry, this._render_tooltips(ds, -1, vars)])
+    }
+
+    if (glyph instanceof HAreaView) {
+      for (const i of subset_indices.line_indices) {
+        const data_x1 = glyph._x1
+        const data_x2 = glyph._x2
+        const data_y = glyph._y
+        const [rx, ry] = [sx, sy]
+        const vars = {
+          index: i,
+          x, y, sx, sy, data_x1, data_x2, data_y, rx, ry,
+          indices: subset_indices.line_indices,
+          name: renderer.name,
+        }
+        tooltips.push([rx, ry, this._render_tooltips(ds, i, vars)])
+      }
+    }
+
+    if (glyph instanceof VAreaView) {
+      for (const i of subset_indices.line_indices) {
+        const data_x = glyph._x
+        const data_y1 = glyph._y1
+        const data_y2 = glyph._y2
+        const [rx, ry] = [sx, sy]
+        const vars = {
+          index: i,
+          x, y, sx, sy, data_x, data_y1, data_y2, rx, ry,
+          indices: subset_indices.line_indices,
+          name: renderer.name,
+        }
+        tooltips.push([rx, ry, this._render_tooltips(ds, i, vars)])
+      }
+    }
 
     if (glyph instanceof LineView) {
       for (const i of subset_indices.line_indices) {

bokehjs/examples/hover/hover.ts
@@ -51,7 +51,8 @@ export namespace HoverfulScatter {
     const div = document.createElement("div")
     div.style.width = "200px"
     div.style.height = "75px"
-    div.style.backgroundColor = ds.data.colors[info.index]
+    if (info.index != null)
+      div.style.backgroundColor = ds.data.colors[info.index]
     return div
   }
 

bokehjs/src/lib/models/glyphs/harea.ts
@@ -54,21 +54,18 @@ export class HAreaView extends AreaView {
 
   protected override _hit_point(geometry: PointGeometry): Selection {
     const L = this.sy.length
-    const sx = new ScreenArray(2*L)
-    const sy = new ScreenArray(2*L)
-
-    for (let i = 0, end = L; i < end; i++) {
-      sx[i] = this.sx1[i]
-      sy[i] = this.sy[i]
-      sx[L+i] = this.sx2[L-i-1]
-      sy[L+i] = this.sy[L-i-1]
-    }
-
     const result = new Selection()
 
-    if (hittest.point_in_poly(geometry.sx, geometry.sy, sx, sy)) {
-      result.add_to_selected_glyphs(this.model)
-      result.view = this
+    for (let i = 0, end = L-1; i < end; i++) {
+      const sx = [this.sx1[i], this.sx1[i+1], this.sx2[i+1], this.sx2[i]]
+      const sy = [this.sy[i], this.sy[i+1], this.sy[i+1], this.sy[i]]
+
+      if (hittest.point_in_poly(geometry.sx, geometry.sy, sx, sy)) {
+        result.add_to_selected_glyphs(this.model)
+        result.view = this
+        result.line_indices = [i]
+        break
+      }
     }
 
     return result

bokehjs/src/lib/models/glyphs/varea.ts
@@ -60,21 +60,18 @@ export class VAreaView extends AreaView {
 
   protected override _hit_point(geometry: PointGeometry): Selection {
     const L = this.sx.length
-    const sx = new ScreenArray(2*L)
-    const sy = new ScreenArray(2*L)
-
-    for (let i = 0, end = L; i < end; i++) {
-      sx[i] = this.sx[i]
-      sy[i] = this.sy1[i]
-      sx[L+i] = this.sx[L-i-1]
-      sy[L+i] = this.sy2[L-i-1]
-    }
-
     const result = new Selection()
 
-    if (hittest.point_in_poly(geometry.sx, geometry.sy, sx, sy)) {
-      result.add_to_selected_glyphs(this.model)
-      result.view = this
+    for (let i = 0, end = L-1; i < end; i++) {
+      const sx = [this.sx[i], this.sx[i+1], this.sx[i+1], this.sx[i]]
+      const sy = [this.sy1[i], this.sy1[i+1], this.sy2[i+1], this.sy2[i]]
+
+      if (hittest.point_in_poly(geometry.sx, geometry.sy, sx, sy)) {
+        result.add_to_selected_glyphs(this.model)
+        result.view = this
+        result.line_indices = [i]
+        break
+      }
     }
 
     return result

commit 981facc8b85034541f64e2e1c5c3c06a2b504e5e

Include `$index` special var in Patch case
Fixes a bug introduced in PR #11992.

bokehjs/src/lib/models/tools/inspectors/hover_tool.ts
@@ -249,12 +249,14 @@ export class HoverToolView extends InspectToolView {
     const tooltips: [number, number, HTMLElement | null][] = []
 
     if (glyph instanceof PatchView) {
+      const i = -1
       const [rx, ry] = [sx, sy]
       const vars = {
+        index: i,
         x, y, sx, sy, rx, ry,
         name: renderer.name,
       }
-      tooltips.push([rx, ry, this._render_tooltips(ds, -1, vars)])
+      tooltips.push([rx, ry, this._render_tooltips(ds, i, vars)])
     }
 
     if (glyph instanceof HAreaView) {