3a9061b4e120c3a2245ba5fa4ac8d4b95d0b557c
Mateusz Paprocki <mattpap@gmail.com>
2022-03-31 16:28:32 +0200

    Rework ridgeplot example to use sub-coordinates

--- examples/plotting/file/ridgeplot.py
+++ examples/plotting/file/ridgeplot.py
@@ -17,29 +17,34 @@ import colorcet as cc
 from numpy import linspace
 from scipy.stats.kde import gaussian_kde
 
-from bokeh.models import ColumnDataSource, FixedTicker, PrintfTickFormatter
+from bokeh.models import ColumnDataSource, Range1d, FixedTicker, PrintfTickFormatter
 from bokeh.plotting import figure, show
 from bokeh.sampledata.perceptions import probly
 
-
-def ridge(category, data, scale=20):
-    return list(zip([category]*len(data), scale*data))
-
 cats = list(reversed(probly.keys()))
-
 palette = [cc.rainbow[i*15] for i in range(17)]
 
-x = linspace(-20,110, 500)
-
+x = linspace(-20, 110, 500)
 source = ColumnDataSource(data=dict(x=x))
 
-p = figure(y_range=cats, width=900, x_range=(-5, 105), toolbar_location=None)
+p = figure(y_range=cats, width=900, x_range=(-5, 105), tools="hover", toolbar_location=None)
+p.hover.tooltips.append(("name", "$name"))
 
 for i, cat in enumerate(reversed(cats)):
+    target_start = cats.index(cat) + 0.5 # middle of the current category
+    target_end = target_start + 20       # arbitrary scaling to make plots pop
+
+    xy = p.subplot(
+        x_source=p.x_range,
+        y_source=Range1d(start=0, end=1),
+        x_target=p.x_range,
+        y_target=Range1d(start=target_start, end=target_end),
+    )
+
     pdf = gaussian_kde(probly[cat])
-    y = ridge(cat, pdf(x))
-    source.add(y, cat)
-    p.patch('x', cat, color=palette[i], alpha=0.6, line_color="black", source=source)
+    source.add(pdf(x), cat)
+
+    xy.patch("x", cat, color=palette[i], alpha=0.6, line_color="black", source=source, name=cat)
 
 p.outline_line_color = None
 p.background_fill_color = "#efefef"
