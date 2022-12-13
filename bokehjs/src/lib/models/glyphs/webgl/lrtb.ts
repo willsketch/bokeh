1e7c80f5838e2ccfe86bda78243ac3834d788f51
Mateusz Paprocki <mattpap@gmail.com>
2022-12-13 11:58:20 +0100

    Improve access to s{left,right,top,bottom} in a loop

--- bokehjs/src/lib/models/glyphs/webgl/lrtb.ts
+++ bokehjs/src/lib/models/glyphs/webgl/lrtb.ts
@@ -29,36 +29,43 @@ export class LRTBGL extends SingleMarkerGL {
     const nmarkers = this.nvertices
 
     if (this._centers == null) {
-      // Either all or none are set.
-      this._centers = new Float32Buffer(this.regl_wrapper)
-      this._widths = new Float32Buffer(this.regl_wrapper)
-      this._heights = new Float32Buffer(this.regl_wrapper)
+      const {regl_wrapper} = this
 
-      this._angles = new Float32Buffer(this.regl_wrapper)
+      // Either all or none are set.
+      this._centers = new Float32Buffer(regl_wrapper)
+      this._widths = new Float32Buffer(regl_wrapper)
+      this._heights = new Float32Buffer(regl_wrapper)
+
+      this._angles = new Float32Buffer(regl_wrapper)
       this._angles.set_from_scalar(0)
     }
 
     const centers_array = this._centers.get_sized_array(nmarkers*2)
     const heights_array = this._heights!.get_sized_array(nmarkers)
     const widths_array = this._widths!.get_sized_array(nmarkers)
-    for (let i = 0; i < nmarkers; i++) {
-      const l = this.glyph.sleft[i]
-      const r = this.glyph.sright[i]
-      const t = this.glyph.stop[i]
-      const b = this.glyph.sbottom[i]
 
-      if (isFinite(l) && isFinite(r) && isFinite(t) && isFinite(b)) {
-        centers_array[2*i] = (l+r)/2
-        centers_array[2*i+1] = (t+b)/2
-        heights_array[i] = Math.abs(t-b)
-        widths_array[i] = Math.abs(r-l)
+    const {sleft, sright, stop, sbottom} = this.glyph
+    const {missing_point} = SingleMarkerGL
+
+    for (let i = 0; i < nmarkers; i++) {
+      const l = sleft[i]
+      const r = sright[i]
+      const t = stop[i]
+      const b = sbottom[i]
+
+      if (isFinite(l + r + t + b)) {
+        centers_array[2*i] = (l + r)/2
+        centers_array[2*i+1] = (t + b)/2
+        heights_array[i] = Math.abs(t - b)
+        widths_array[i] = Math.abs(r - l)
       } else {
-        centers_array[2*i] = SingleMarkerGL.missing_point
-        centers_array[2*i+1] = SingleMarkerGL.missing_point
-        heights_array[i] = SingleMarkerGL.missing_point
-        widths_array[i] = SingleMarkerGL.missing_point
+        centers_array[2*i] = missing_point
+        centers_array[2*i+1] = missing_point
+        heights_array[i] = missing_point
+        widths_array[i] = missing_point
       }
     }
+
     this._centers.update()
     this._heights!.update()
     this._widths!.update()
